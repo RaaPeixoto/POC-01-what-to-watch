624976821498952c93d53b41784a56a0b41a611f
Raísa Peixoto <raisacad.engcivil@gmail.com>
2023-01-22 21:14:13 -0300

    del: build

--- build/routers/moviesRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var moviesController_1 = require("../controllers/moviesController");
-var router = (0, express_1.Router)();
-router.get("/posts/:plataform/:genre", moviesController_1.getMovies);
-exports.default = router;
